59625f3e0033ff4e2e9d7bc8d5091261191feebb
Zavaxe <zavax94@gmail.com>
2022-10-19 23:11:37 +0300

    Calculate the total time of the selected songs from the list and dictionary

--- 06_songs_list.py
+++ 06_songs_list.py
@@ -16,11 +16,12 @@ violator_songs_list = [
 ]
 
 # распечатайте общее время звучания трех песен: 'Halo', 'Enjoy the Silence' и 'Clean' в формате
-#   Три песни звучат ХХХ минут
+# Три песни звучат ХХХ минут
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
-# TODO здесь ваш код
+songs_time = violator_songs_list[3][1] + violator_songs_list[5][1] + violator_songs_list[-1][1]
+print('Три песни звучат', round(songs_time, 2), 'минут')
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -38,4 +39,6 @@ violator_songs_dict = {
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат ХХХ минут
 
-# TODO здесь ваш код
+other_3songs = violator_songs_dict['World in My Eyes'] \
+               + violator_songs_dict['Policy of Truth'] + violator_songs_dict['Blue Dress']
+print('А другие три песни звучат',other_3songs,'минут')
