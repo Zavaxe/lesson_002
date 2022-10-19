36bbbbeddb48c31e9ce247428b816a2387d556af
Zavaxe <zavax94@gmail.com>
2022-10-19 19:41:40 +0300

    Created a family list, list of list included name and height. Printd the height of the father and summ height of the family

--- 04_my_family.py
+++ 04_my_family.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 # Создайте списки:
 
 # моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
-my_family = []
+my_family = ['mother', 'father', 'brother','sister']
 
 
 # список списков приблизителного роста членов вашей семьи
 my_family_height = [
     # ['имя', рост],
-    [],
+    ['Alla', 160], ['Sergey', 180], ['Aleksandr', 183], ['Evgeniya', 174]
 ]
 
 # Выведите на консоль рост отца в формате
@@ -18,3 +16,7 @@ my_family_height = [
 
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
+
+print('Рост отца -', my_family_height[1][1], 'см')
+overall_height = my_family_height[0][1] + my_family_height[1][1] + my_family_height[2][1] + my_family_height[3][1]
+print('Общий рост моей семьи -',overall_height,'см')
